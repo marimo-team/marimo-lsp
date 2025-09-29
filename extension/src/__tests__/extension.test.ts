ec8c1e57e2a80ad17ed4e76972bb727e4b27208b
Trevor Manz <trevor.j.manz@gmail.com>
2025-09-28 21:23:22 -0400

    Use `.ts` extension

--- extension/src/__tests__/extension.test.ts
+++ extension/src/__tests__/extension.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it, vi } from "vitest";
-import { createVSCodeMock } from "../__mocks__/vscode";
+import { createVSCodeMock } from "../__mocks__/vscode.ts";
 
 vi.mock("vscode", () => createVSCodeMock(vi));
 vi.mock("vscode-languageclient/node", () => createVSCodeLanguageClientMock(vi));
