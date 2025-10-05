d33ea3439d5fb37c9b52030d6291c74ea4b30723
Trevor Manz <trevor.j.manz@gmail.com>
2025-10-05 07:50:37 -0400

    Make `TestLanguageClient` lazy

--- extension/src/__tests__/TestLanguageClient.ts
+++ extension/src/__tests__/TestLanguageClient.ts
@@ -1,5 +1,5 @@
 import * as NodeChildProcess from "node:child_process";
-import { Effect, Layer, Stream } from "effect";
+import { Effect, Layer, Option, Ref, Stream } from "effect";
 import * as rpc from "vscode-jsonrpc/node";
 import {
   ExecuteCommandError,
@@ -8,62 +8,78 @@ import {
   LanguageClientStartError,
 } from "../services/LanguageClient.ts";
 
-export const TestLanguageClientLive = Layer.scoped(
+export const TestLanguageClientLive = Layer.effect(
   LanguageClient,
   Effect.gen(function* () {
-    const { command, args } = yield* findLspExecutable();
-    const proc = NodeChildProcess.spawn(command, args, {
-      stdio: ["pipe", "pipe", "inherit"],
-    });
-    const connection = rpc.createMessageConnection(
-      new rpc.StreamMessageReader(proc.stdout),
-      new rpc.StreamMessageWriter(proc.stdin),
+    const ref = yield* Ref.make<Option.Option<rpc.MessageConnection>>(
+      Option.none(),
     );
     return LanguageClient.make({
       manage: () =>
         Effect.acquireRelease(
           Effect.gen(function* () {
-            connection.listen();
+            const exec = yield* findLspExecutable();
+            const proc = NodeChildProcess.spawn(exec.command, exec.args, {
+              stdio: ["pipe", "pipe", "inherit"],
+            });
+            const conn = rpc.createMessageConnection(
+              new rpc.StreamMessageReader(proc.stdout),
+              new rpc.StreamMessageWriter(proc.stdin),
+            );
+            conn.listen();
             yield* Effect.tryPromise({
               try: () =>
-                connection.sendRequest("initialize", {
+                conn.sendRequest("initialize", {
                   processId: process.pid,
                   capabilities: {},
                 }),
               catch: (cause) =>
                 new LanguageClientStartError({
-                  exec: { command, args },
+                  exec,
                   cause,
                 }),
             });
             yield* Effect.promise(() =>
-              connection.sendNotification("initialized", {}),
+              conn.sendNotification("initialized", {}),
             );
+            yield* Ref.set(ref, Option.some(conn));
+            return { conn, proc };
           }),
-          () =>
-            Effect.sync(() => {
-              connection.dispose();
+          ({ conn, proc }) =>
+            Effect.gen(function* () {
+              conn.dispose();
               proc.kill();
+              yield* Ref.set(ref, Option.none());
             }),
         ),
       executeCommand(cmd) {
-        return Effect.tryPromise({
-          try: () =>
-            connection.sendRequest("workspace/executeCommand", {
-              command: cmd.command,
-              arguments: [cmd.params],
-            }),
-          catch: (cause) => new ExecuteCommandError({ command: cmd, cause }),
+        return Effect.gen(function* () {
+          const conn = Option.getOrThrowWith(
+            yield* Ref.get(ref),
+            () => new Error("Language server connection closed."),
+          );
+          return yield* Effect.tryPromise({
+            try: () =>
+              conn.sendRequest("workspace/executeCommand", {
+                command: cmd.command,
+                arguments: [cmd.params],
+              }),
+            catch: (cause) => new ExecuteCommandError({ command: cmd, cause }),
+          });
         });
       },
       streamOf(notification) {
         return Stream.asyncPush((emit) =>
           Effect.acquireRelease(
-            Effect.sync(() =>
-              connection.onNotification(notification, (msg) =>
+            Effect.gen(function* () {
+              const conn = Option.getOrThrowWith(
+                yield* Ref.get(ref),
+                () => new Error("Language server connection closed."),
+              );
+              return conn.onNotification(notification, (msg) =>
                 emit.single(msg),
-              ),
-            ),
+              );
+            }),
             (disposable) => Effect.sync(() => disposable.dispose()),
           ),
         );
