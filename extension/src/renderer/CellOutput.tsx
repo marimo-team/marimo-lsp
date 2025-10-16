3680504ac410b0b0f1cced437bab62fe3706dd72
Myles Scolnick <myles@marimo.io>
2025-10-16 16:59:32 -0400

    updates

--- extension/src/renderer/CellOutput.tsx
+++ extension/src/renderer/CellOutput.tsx
@@ -18,8 +18,13 @@ interface CellOutputProps {
 export function CellOutput({ cellId, state }: CellOutputProps) {
   const { theme } = useTheme();
   const container = useRef<HTMLDivElement>(null);
+
   return (
-    <div className={`marimo-cell-output p-4 pt-6 ${theme}`} ref={container}>
+    <div
+      className={`marimo-cell-output p-4 pt-6 ${theme}`}
+      data-vscode-output-container="true"
+      ref={container}
+    >
       <TooltipProvider container={container.current}>
         {state.output && (
           <OutputRenderer cellId={cellId} message={state.output} />
