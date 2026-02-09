aa4f32e41d739428232db45ead324b40d324a8f5
Trevor Manz <trevor.j.manz@gmail.com>
2026-02-09 12:00:24 -0500

    Upgrade ty to v0.0.15

--- extension/src/services/completions/TyLanguageServer.ts
+++ extension/src/services/completions/TyLanguageServer.ts
@@ -32,7 +32,7 @@ import {
 
 // TODO: make TY_VERSION configurable?
 // For now, since we are doing rolling releases, we can bump this as needed.
-const TY_SERVER = { name: "ty", version: "0.0.14" } as const;
+const TY_SERVER = { name: "ty", version: "0.0.15" } as const;
 
 export const TyLanguageServerStatus = Data.taggedEnum<TyLanguageServerStatus>();
 
